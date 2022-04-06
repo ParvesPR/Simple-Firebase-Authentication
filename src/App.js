ce691fab22488e61a4f2acd842067c1123d58ab8
ParvesPR <parves2585@gmail.com>
2022-04-06 23:36:58 +0800

    sign out button added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import './App.css';
 import app from './firebase.init';
-import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
+import { useState } from 'react';
 
 const auth = getAuth(app)
 
 function App() {
   const provider = new GoogleAuthProvider();
+  const [users, setUsers] = useState({})
   const handleGoogleAuth = () => {
     signInWithPopup(auth, provider)
       .then(result => {
         const user = result.user;
+        setUsers(user)
         console.log(user)
       })
       .catch(error => {
         console.log('error', error)
       })
   }
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(
+        setUsers({})
+      )
+      .catch(error => { })
+  }
   return (
     <div className="App">
+      <button onClick={handleSignOut}>Sign Out</button>
       <button onClick={handleGoogleAuth}>Google Sign in</button>
+      <h2>Name: {users.displayName}</h2>
+      <h3>Email: {users.email}</h3>
+      <img src={users.photoURL} alt="" />
     </div>
   );
 }

f62b70489778e8818dfb804b5d5040d9cf758768
ParvesPR <parves2585@gmail.com>
2022-04-06 14:31:03 +0800

    create google sign in btn

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import app from './firebase.init';
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+
+const auth = getAuth(app)
 
 function App() {
+  const provider = new GoogleAuthProvider();
+  const handleGoogleAuth = () => {
+    console.log('warning')
+  }
   return (
     <div className="App">
-
+      <button onClick={handleGoogleAuth}>Google</button>
     </div>
   );
 }

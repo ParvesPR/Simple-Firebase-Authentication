db4be22d1e31d2ee09269d7be0f9ef3d5230a468
ParvesPR <parves2585@gmail.com>
2022-04-06 15:11:22 +0800

    button event listner added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import './App.css';
 import app from './firebase.init';
-import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 
 const auth = getAuth(app)
 
 function App() {
   const provider = new GoogleAuthProvider();
   const handleGoogleAuth = () => {
-    console.log('warning')
+    signInWithPopup(auth, provider)
+      .then(result => {
+        const user = result.user;
+        console.log(user)
+      })
+      .catch(error => {
+        console.log('error', error)
+      })
   }
   return (
     <div className="App">
-      <button onClick={handleGoogleAuth}>Google</button>
+      <button onClick={handleGoogleAuth}>Google Sign in</button>
     </div>
   );
 }
